9cb7ad4e8bf6d071f6f85da14290360d4afc8097
Rafael Ferreira <rafaelsf@gmail.com>
2017-12-27 16:10:58 -0600

    feat: Show story summary on the markdown

--- src/fmt/markdown.rs
+++ src/fmt/markdown.rs
@@ -1,10 +1,18 @@
+extern crate regex;
+
+use regex::Regex;
 use changelog::Changelog;
 
 pub fn create_from(changelog: &Changelog) {
   println!("## {:?}, {}", changelog.title, changelog.created_at);
 
+  println!("\n## Story Summary");
+  changelog.stories(&Regex::new(r"^test").unwrap())
+    .iter()
+    .for_each(|story| println!("* {}", story));
+
   for (tag, commits) in changelog.commits_by_tag() {
-    println!("### {}", tag);
-    commits.iter().for_each(|commit| println!("* {} ({})", commit.subject, commit.hash))
+    println!("\n### {}", tag);
+    commits.iter().for_each(|commit| println!("* {} ({})", commit.subject, commit.hash));
   }
 }
