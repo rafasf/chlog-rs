ae283956c23e285d00387095ee8737a04d5536b8
Rafael Ferreira <rafaelsf@gmail.com>
2018-09-19 12:09:05 -0500

    Add optional component for default tags
    All the default tags will match by itself or with a component.
    
    Example: defaultTag, defaultTag(component).
    
    feat(tag)

--- src/config.rs
+++ src/config.rs
@@ -13,11 +13,12 @@ impl Config {
 
         Config {
             tags: vec![
-                Tag::from(r"(chore):\s*", "Chore"),
-                Tag::from(r"(doc):\s*", "Documentation"),
-                Tag::from(r"(style):\s*", "Style"),
-                Tag::from(r"feat\(?(\w+)?\)?:\s*", "Feature"), //example of optional component
-                Tag::from(r"(refactor):\s*", "Refactor"),
+                Tag::from(r"chore\(?(\w+)?\)?:?\s*", "Chore"),
+                Tag::from(r"doc\(?(\w+)?\)?:?\s*", "Documentation"),
+                Tag::from(r"style\(?(\w+)?\)?:?\s*", "Style"),
+                Tag::from(r"refactor\(?(\w+)?\)?:?\s*", "Refactor"),
+                Tag::from(r"feat\(?(\w+)?\)?:?\s*", "Feature"),
+                Tag::from(r"ci\(?(\w+)?\)?:?\s*", "CI"),
             ],
             format: format!("--pretty=format:%s{s}%an{s}%h{s}%b", s = separator),
             separator: separator.to_string(),
